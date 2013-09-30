157df1747420eb8efba95c0baf900e8d11ea18eb
Friedrich Lindenberg <friedrich@pudo.org>
2013-09-30 13:01:38 +0545

    Content negotiation, support for JSON and JSONP

--- databin/util.py
+++ databin/util.py
@@ -1,13 +1,35 @@
-from flask import make_response
+from flask import make_response, request
 from StringIO import StringIO
 from uuid import uuid4
+from hashlib import sha1
 import csv
+import json
 
 
-def make_it_cache(res, timestamp):
-    res.headers['Cache-Control'] = 'public; max-age: 86400'
-    res.headers['Last-Modified'] = timestamp.strftime('%a, %d %b %Y %H:%M:%S GMT')
+FORMATS = {
+    'application/json': 'json',
+    'text/csv': 'csv',
+    'text/html': 'html'
+    }
+
+
+def response_format(url_format):
+    if url_format is not None and url_format in FORMATS.values():
+        return url_format
+    best_mime = request.accept_mimetypes.best_match(FORMATS.keys(),
+            default='text/html')
+    return FORMATS.get(best_mime)
+
+
+def make_it_cache(res, etag):
+    res.headers['Cache-Control'] = 'public; max-age: 8640000'
+    res.headers['ETag'] = etag
     return res
+
+
+def generate_etag(key, format):
+    buf = '%s//%s//%s' % (key, format, request.args.get('callback'))
+    return '"%s"' % sha1(buf).hexdigest()
 
 
 def make_csv(table):
@@ -20,8 +42,18 @@ def make_csv(table):
     return res
 
 
-def decode(number):
-    return int(number, 36)
+def make_json(table, has_header):
+    data = {'header': [], 'rows': list(table)}
+    if has_header and len(data['rows']):
+        data['header'] = data['rows'][0]
+        data['rows'] = data['rows'][1:]
+    data = json.dumps(data)
+    if 'callback' in request.args:
+        data = '%s && %s(%s);' % (request.args.get('callback'), 
+                request.args.get('callback'), data)
+    res = make_response(data)
+    res.headers['Content-Type'] = 'application/json; encoding=utf-8'
+    return res
 
 
 def make_key():
--- databin/web.py
+++ databin/web.py
@@ -2,45 +2,53 @@ import logging
 
 from flask import render_template, request, redirect, url_for
 from flask import make_response
-from werkzeug.exceptions import NotFound
+from werkzeug.exceptions import NotFound, HTTPException
 from formencode import Invalid, htmlfill
 
 from databin.core import app
 from databin.model import Paste
-from databin.util import make_it_cache, make_csv
+from databin.util import make_it_cache, make_csv, make_json
+from databin.util import generate_etag, response_format
 from databin.parsers import get_parsers, ParseException, parse
 
 log = logging.getLogger(__name__)
 
+class NotModified(HTTPException):
+    code = 304
 
-def get_paste(key):
+
+def get_paste(key, format):
     paste = Paste.by_key(key)
     if paste is None:
         raise NotFound('No such table: %s' % key)
+    etag = generate_etag(key, format)
+    if request.if_none_match and request.if_none_match == etag:
+        raise NotModified()
     has_header, table = False, None
     try:
         has_header, table = parse(paste.format, paste.data)
     except ParseException, pe:
         log.exception(pe)
     has_header = has_header or paste.force_header
-    return paste, table, has_header
+    return paste, table, has_header, etag
 
 
+@app.route("/t/<key>.<format>")
 @app.route("/t/<key>")
-def view(key):
-    paste, table, has_header = get_paste(key)
-    html = render_template('view.html',
-                           paste=paste.to_dict(),
-                           has_header=has_header,
-                           table=table)
-    return make_it_cache(make_response(html),
-                         paste.created_at)
-
-
-@app.route("/c/<key>")
-def view_csv(key):
-    paste, table, has_header = get_paste(key)
-    return make_it_cache(make_csv(table), paste.created_at)
+def view(key, format=None):
+    format = response_format(format)
+    paste, table, has_header, etag = get_paste(key, format)
+    if format == 'json':
+        res = make_json(table, has_header)
+    elif format == 'csv':
+        res = make_csv(table)
+    else:
+        html = render_template('view.html',
+                               paste=paste.to_dict(),
+                               has_header=has_header,
+                               table=table)
+        res = make_response(html)
+    return make_it_cache(res, etag)
 
 
 @app.route("/", methods=['POST'])
